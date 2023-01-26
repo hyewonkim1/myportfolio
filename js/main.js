4f8b1bc73d49b7c53aa0bb6e6fee471dfe13d7b9
hyewonkim <hw4688@naver.com>
2023-01-26 17:03:46 +0900

    portfolio ver7

--- js/main.js
+++ js/main.js
@@ -113,43 +113,38 @@ $(function(){
 
 
 // 타이핑 효과
-// 텍스트 요소 
+
 const $text = document.querySelector(".text");
-// 글자 모음 - 개행문자(\n)로 줄바꿈
 const letters = [
   "HYEWON \n PORTFOLIO!"
 ];
-// 글자 입력 속도
 const speed = 100;
-// 현재 지정된 글자 
-let i = 0;
-// 줄바꿈을 위한 <br> 
+let i = 0; 
 const changeLineBreak = (letter) => {
   return letter.map(text => text === "\n" ? "<br>" : text);
 }
-// 타이핑 기능 ( wait 기능을 위한 async, await )
+
 const typing = async () => {  
-  // 기존코드에서 개행치환코드 추가
+
   const letter = changeLineBreak(letters[i].split(""));
   
   while (letter.length) {
-    // 글자 작성 속도 조절 
+
     await wait(speed);
     $text.innerHTML += letter.shift(); 
   }
-  // 작성 된 후 잠시 멈추기 
+
   await wait(1000);
 }
 
-// 딜레이 기능 ( 마이크로초 )
-// 텍스트가 작성되는 속도나 작성되고 텍스트가 멈추는 시간을 구현하기 위해 setTimeout함수로 딜레이를 주고, promise를 이용해서 딜레이 기능을 주는 함수 구현. 
+
 
 function wait(ms) {
   return new Promise(res => setTimeout(res, ms))
-  // reslove 
+
 }
 
-// 초기 실행
+
 setTimeout(typing, 500);
 
 
