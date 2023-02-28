3887596ce9c01448c2abd41371e5d87f90428db6
hw_kim <hw4688@naver.com>
2023-02-28 12:38:40 +0900

    portfolio

--- js/main.js
+++ js/main.js
@@ -1,87 +1,27 @@
 $(function(){
 
-  $('.wrap section').mousewheel(function(e,delta){
-    if(delta > 0) { 
-      // mousewhell up
-      let prev = $(this).prev().offset().top;
-      $('html, body').stop().animate({scrollTop:prev},1000);
-    } else if(delta < 0) {
-      let next = $(this).next().offset().top;
-      $('html, body').stop().animate({scrollTop:next},1000);
-    }
-  }) //mousewheel event
 
-  $('.lnb>a').click(function(e){
+  $('.lnb a').click(function(e){
     e.preventDefault();
     $(window).scrollTo(this.hash || 0, 1000); 
   }) // lnb 
 
 
   $(window).scroll(function(){
-    $('.wrap>section').each(function(){
+    $('.wrap section').each(function(){
       let sectionPoint = $(window).scrollTop();
       let target = $(this);
       let i = target.index();
-      let lnbLi = $('.lnb>li');
+      let lnbLi = $('.lnb li');
 
       if(sectionPoint >= $(this).offset().top) {
         lnbLi.removeClass('on');
         lnbLi.eq(i).addClass('on');
       }
     })
-  })
+  }) //lnb on 
 
 
-  $(window).scroll(function(){
-
-    if($(this).scrollTop() == $('.page02').offset().top) {
-      $('.sub_box02').fadeIn(500);
-    } else {
-      $('.sub_box02').fadeOut();
-
-    }
-
-    if($(this).scrollTop() == $('.page03').offset().top) {
-      $('.page03 .sub_box03').fadeIn(500);
-    } else {
-      $('.page03 .sub_box03').fadeOut();
-    }
-
-    if($(this).scrollTop() == $('.page04').offset().top) {
-      $('.page04-1').fadeIn(500);
-      $('.page04-2').delay(500).fadeIn(500);
-    } else {
-      $('.page04-1').fadeOut();
-      $('.page04-2').fadeOut();
-    }
-
-    if($(this).scrollTop() == $('.page05').offset().top) {
-      $('.page05-1').fadeIn(500);
-      $('.page05-2').delay(500).fadeIn(500);
-    } else {
-      $('.page05-1').fadeOut();
-      $('.page05-2').fadeOut();
-    }
-
-    if($(this).scrollTop() == $('.page06').offset().top) {
-      $('.page06 .sub_box06').fadeIn(500);
-    } else {
-      $('.page06 .sub_box06').fadeOut();
-    }
-
-    if($(this).scrollTop() == $('.page07').offset().top) {
-      $('.page07-1').fadeIn(500);
-      $('.page07-2').delay(500).fadeIn(500);
-    } else {
-      $('.page07-1').fadeOut();
-      $('.page07-2').fadeOut();
-    }
-
-    if($(this).scrollTop() == $('.page08').offset().top) {
-      $('.page08 .sub_box08').fadeIn(500);
-    } 
-
-  }) // window scroll event
 
   $('.top_btn').click(function(e){
     e.preventDefault();
@@ -109,7 +49,7 @@ $(function(){
 
 
   
-})
+
 
 
 // 타이핑 효과
@@ -191,4 +131,4 @@ document.addEventListener('DOMContentLoaded',() => {
   
   
 })
-
+})
